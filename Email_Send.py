6197e374e5f7b900e529b566883e030bd072b661
Harshal Kotadiya <hkotadiya1@gmail.com>
2022-01-05 21:46:31 +0530

    Update Email_Send.py

--- Email_Send.py
+++ Email_Send.py
@@ -3,7 +3,7 @@
 # from email.message import EmailMessage # Import the email modules
 # email = EmailMessage()
 # email['to'] = "Harshal Kotadiya"
-# email['from'] = 'hkotadiya001@gmail.com'
+# email['from'] = 'Email'
 # email['subject'] = "Hello! I am new here. "
 #
 # email.set_content('You got me!')
@@ -12,14 +12,14 @@
 #
 #     s.ehlo()
 #     s.starttls()
-#     s.login('hkotadiya001@gmail.com', 'Patel@92')  # Turn on less secure account ON
+#     s.login('********************', '******')  # Turn on less secure account ON
 
 *******************************************************************************
 import smtplib, ssl
 
 smtp_server = "smtp.gmail.com"
 port = 587  # For starttls
-sender_email = "hkotadiya001@gmail.com"
+sender_email = "email id"
 password = input("Type your password and press enter: ")
 
 # Create a secure SSL context
